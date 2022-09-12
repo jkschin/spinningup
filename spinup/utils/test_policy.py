16149dfee93984f501535edea5cf9b3de29843c4
Samuel Chin <samuelchin91@gmail.com>
2022-09-12 17:26:12 -0400

    Saving videos still has an issue

--- spinup/utils/test_policy.py
+++ spinup/utils/test_policy.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 import torch
 from spinup import EpochLogger
 from spinup.utils.logx import restore_tf_graph
+from gym.utils.save_video import save_video
 
 
 def load_policy_and_env(fpath, itr='last', deterministic=False):
@@ -131,6 +132,15 @@ def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
         ep_len += 1
 
         if d or (ep_len == max_ep_len):
+            frames = env.render()
+            # print(len(frames))
+            # save_video(
+            #     frames,
+            #     "videos",
+            #     fps=240,
+            #     episode_index=n
+            # )
+            # frames = []
             logger.store(EpRet=ep_ret, EpLen=ep_len)
             print('Episode %d \t EpRet %.3f \t EpLen %d' % (n, ep_ret, ep_len))
             o, info = env.reset()
